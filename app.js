85ee3e1b5b22638697a3ea7606b0948b6aac3632
runtee <ekwo.chinonso.01@gmail.com>
2022-06-04 12:31:28 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const  cors = require("cors");
+const uuid = require("uuid");
+const fs = require("fs");
+const path = require("path")
+require('dotenv').config();
+const inventoriesRoutes = require('./routes/inventories');
+const warehousesRoutes = require('./routes/warehouses');
+
+
+app.use(express.json());
+app.use(cors());
+
+app.use('/inventory', inventoriesRoutes);
+app.use('/warehouse', warehousesRoutes);
+
+
+app.listen(5000, (error) => {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log("Server Running at port 5000...")
+    }})
